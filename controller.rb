efa42176128c518be946a8f209422ae5ac62a1f1
Kenny_and_Tony <apprentice@dbc-chicago-63.local>
2014-02-28 11:14:23 -0600

    adds FlashcardsController class to the controller

--- controller.rb
+++ controller.rb
@@ -1,2 +1,17 @@
 require_relative 'model'
 require_relative 'view'
+
+class FlashcardsController
+  attr_accessor :deck
+
+  def initialize(filename)
+    @deck = Deck.new(filename)
+  end
+
+  def run
+    Welcome.new.welcome
+    until deck.empty?
+      #so stuff
+    end
+  end
+end
